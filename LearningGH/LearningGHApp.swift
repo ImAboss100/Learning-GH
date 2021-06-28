a081f4edc2cfb88ef7c35dea6688cc06e587de51
jordan <jpballer555@yahoo.com>
2021-06-28 12:53:49 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Jordan Perez on 6/28/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
